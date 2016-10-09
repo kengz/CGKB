d5da156b740b85ce7296be2f0e1e29d973a648d7
kengz <kengzwl@gmail.com>
2016-10-09 17:36:49 -0400

    mute tests till further need

--- test/test.js
+++ test/test.js
@@ -1,14 +1,12 @@
 const Promise = require('bluebird')
 const chai = require('chai')
 const should = chai.should()
-const polyIO = require('poly-socketio')
-const startIO = require('../src/start-io')
 
-describe('start poly-socketio', () => {
-  var ioPromise = startIO()
+// describe('start poly-socketio', () => {
+//   var ioPromise = startIO()
 
-  it('resolve global.ioPromise when all joined', () => {
-    globalClient = polyIO.gClient({ port: process.env.IOPORT })
-    return ioPromise
-  })
-})
+//   it('resolve global.ioPromise when all joined', () => {
+//     globalClient = polyIO.gClient({ port: process.env.IOPORT })
+//     return ioPromise
+//   })
+// })
