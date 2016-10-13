9474207a60a0a0cbfbbe6c0836b53c93eb5ae594
kengz <kengzwl@gmail.com>
2016-10-13 00:43:44 -0400

    cleanup n export kb

--- src/kb.js
+++ src/kb.js
@@ -1,15 +1,6 @@
-const Promise = require('bluebird')
 const _ = require('lodash')
 const path = require('path')
 const db = require(path.join(__dirname, 'db'))
-
-// suggested prop keys
-var prop = {
-  label: 'entity type:, PERSON etc. Easy for edge visualization',
-  name: 'easy for node visualization'
-  type: 'data type',
-  // <any key>: as long as you keep track of the schema
-}
 
 // -------------------------------------------------------------
 // helpers
@@ -175,82 +166,20 @@ function clearTestDb() {
 // clearDb()
 //   .then(console.log)
 
-// -------------------------------------------------------------
-// demo
 
-var propA = { name: "Alice", email: "alice@example.com", label: "PERSON" }
-var propB = { name: "Bob", email: "bob@example.com", label: "PERSON" }
-var propAB = { name: "love-love", label: "LOVE" }
-var propBA = { name: "friend-love", label: "LOVE" }
-var graph = [
-  [propA, propB, propAB],
-  [propB, propA, propBA]
-]
-
-// var qp = addGraph(graph)
-// var qp = getGraph(graph)
-// var qp = removeGraph([propA, propB])
-
-db.cypherAsync(qp)
-  .then((res) => {
-    console.log(res)
-  })
-
-// var label = "PERSON"
-// var prop = { name: "Alice", email: "alice@example.com", label: label }
-// var setProp = { name: "Alice", email: "bob@example.com", label: "STORY" }
-// var timestamp = new Date().toJSON()
-// var edgeProp = { name: "Evolution", label: "BECOME" }
-// var setEdgeProp = { name: "Devolution", label: "WAS" }
-
-// ohh shit allow for label update too
-// var qp = addNode(prop, timestamp)
-// var qp = getNode(prop)
-// var qp = removeNode(prop)
-// var qp = updateNode(prop, setProp, timestamp)
-// var qp = addEdge(prop, setProp, edgeProp)
-// var qp = getEdge(prop, setProp, edgeProp)
-// var qp = updateEdge(prop, setProp, edgeProp, setEdgeProp)
-// var qp = removeEdge(prop, setProp, {})
-// console.log(qp)
-
-// db.cypherAsync(qp)
-//   .then((res) => {
-//     console.log(res)
-//   })
-
-
-// yeah can make a direct parse tree in graph
-// var parseTrees = [{
-//   "word": "like",
-//   "lemma": "like",
-//   "NE": "",
-//   "POS_fine": "VBP",
-//   "POS_coarse": "VERB",
-//   "arc": "ROOT",
-//   "modifiers": [{
-//     "word": "I",
-//     "lemma": "i",
-//     "NE": "",
-//     "POS_fine": "PRP",
-//     "POS_coarse": "PRON",
-//     "arc": "nsubj",
-//     "modifiers": []
-//   }, {
-//     "word": "apple",
-//     "lemma": "apple",
-//     "NE": "",
-//     "POS_fine": "NN",
-//     "POS_coarse": "NOUN",
-//     "arc": "dobj",
-//     "modifiers": []
-//   }, {
-//     "word": ".",
-//     "lemma": ".",
-//     "NE": "",
-//     "POS_fine": ".",
-//     "POS_coarse": "PUNCT",
-//     "arc": "punct",
-//     "modifiers": []
-//   }]
-// }]
+module.exports = {
+  db: db,
+  addNode: addNode,
+  getNode: getNode,
+  updateNode: updateNode,
+  removeNode: removeNode,
+  addEdge: addEdge,
+  getEdge: getEdge,
+  updateEdge: updateEdge,
+  removeEdge: removeEdge,
+  addGraph: addGraph,
+  getGraph: getGraph,
+  removeGraph: removeGraph,
+  clearDb: clearDb,
+  clearTestDb: clearTestDb
+}
